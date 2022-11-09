a3c0342658c2906d3f6de0c5b2bb9842ffd32c0a
Sofia-Citrini <sofiacitrini@gmail.com>
2022-11-09 20:10:05 +0100

    bonus with 3 grids

--- js/main.js
+++ js/main.js
@@ -1,35 +1,45 @@
 const btnPlay = document.getElementById('btn-play');
-const inputDifficulty = document.getElementById("[name='difficulty']");
+
 
 btnPlay.addEventListener('click', function () {
     console.log(this);
-    createGrid(10);
+
+    const inputDifficulty = document.querySelector("[name='difficulty']");
+
+    let gridCells = '';
+    if (inputDifficulty.value === '1') {
+        gridCells = 100;
+    } else if (inputDifficulty.value === '2') {
+        gridCells = 81;
+    } else if (inputDifficulty.value === '3') {
+        gridCells = 49;
+    }
+
+    createGrid (gridCells);
 })
 
-function createGrid(celle) {
+function createGrid (celleTotali) {
     const grid = document.getElementById('grid-container');
 
-    // celle totali
-    const gridCells = Math.pow(celle, 2);
+    grid.innerHTML = "";
 
-    for (let i = 0; i < gridCells; i++) {
+    // celle per ogni riga
+    const cellRow = Math.sqrt(celleTotali);
+
+    for (let i = 0; i < celleTotali; i++) {
         // creare elemento html
         const newCell = document.createElement('div');
-
         newCell.classList.add("cell");
-        newCell.style.flexBasis = 100 / celle + '%';
+        newCell.style.flexBasis = 100 / cellRow + '%';
 
         // numero progressivo da 1 a 100
         newCell.innerHTML = i + 1;
+        grid.append(newCell);
 
         // click:cambio colore + messaggio in console 
         newCell.addEventListener('click', function () {
             newCell.classList.toggle("bg-primary");
-
-            console.log(i + 1)
+            console.log(this.textContent);
         })
-
-        grid.append(newCell);
     }
 }
-
